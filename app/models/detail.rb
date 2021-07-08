a942b766b2d01f75cbbe7a600ec7beb80c79de93
Divyanshu <divyanshujain20010808@gmail.com>
2021-07-08 18:59:26 +0530

    added validations

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,5 +1,5 @@
 class Detail < ApplicationRecord
   belongs_to :user
   validates :department, presence: true
-  validates :roll_no, presence: true
+  validates :roll_no, presence: true, lenght: { is: 13}
 end
